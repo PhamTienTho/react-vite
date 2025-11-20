6b4b779e994a984ffd79823cad6678298083a68c
Phạm Tiến Thọ <tientho1420@gmail.com>
2025-11-21 01:58:37 +0700

    video-30: JSX/Fragment

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,15 @@
 
+import './style.css';
+
 // component = html + css + js
 const MyComponent = () => {  // tên component phải viết hoa chữ cái đầu
-  return (
-    <div> First component ver 2.0 </div>
-  );
+    return (
+        // sử dụng Fragment để bọc giúp cho có thể return nhiều thẻ html trong 1 component
+        <>
+            <div> First component ver 2.0 </div>
+            <div className="child">child</div>
+        </>
+    );
 }
 
 export default MyComponent // default có ý nghĩa là file này luôn mặc định là export ra component này
