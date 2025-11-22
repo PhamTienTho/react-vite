b6f674f5320592a3507d3b2b7528208b1aa145b7
Phạm Tiến Thọ <tientho1420@gmail.com>
2025-11-23 01:22:01 +0700

    video-31: sử dụng biến số với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,11 +3,14 @@ import './style.css';
 
 // component = html + css + js
 const MyComponent = () => {  // tên component phải viết hoa chữ cái đầu
+    const str1 = "tho";
+    const num1 = 5;
     return (
         // sử dụng Fragment để bọc giúp cho có thể return nhiều thẻ html trong 1 component
         <>
-            <div> First component ver 2.0 </div>
+            <div> {str1} {num1} First component ver 2.0 </div>
             <div className="child">child</div>
+            <div> { console.log("tho")} </div>
         </>
     );
 }
