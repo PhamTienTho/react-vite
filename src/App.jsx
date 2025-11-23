aaa8ca17f7d6d7e289cec496472d20c5783f7b86
Phạm Tiến Thọ <tientho1420@gmail.com>
2025-11-23 16:04:26 +0700

    video-34: truyền pops từ component cha sang con, sử dụng destructuring object

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import logoReact from './assets/react.svg';
 
+
+const myName = "thodz";
+const age = 21;
+const myData = {
+  country: "Viet Nam",
+  address: "Ha Noi"
+}
+
 const App = () => {
   return (
     <div className="todo-container">
       <div className="todo-title">To do List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData 
+        name = {myName}
+        age = {age}
+        data = {myData}
+      />
       <div className='todo-image'>
         <img className='logo' src={logoReact}/>
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,13 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    // props là 1 biến object
+
+
+    const {name , age, data} = props; // sử dụng destructuring object
+
+    console.log(">>> check props: ", props);
     return (
         <div className="todo-data">
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
