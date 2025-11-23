2148c0569d4a1b5da90f7bdb136ef333bbb28ba0
Phạm Tiến Thọ <tientho1420@gmail.com>
2025-11-24 00:42:33 +0700

    video-36: truyền tham số vào Event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,15 +1,28 @@
 
 
 const TodoNew = (props) => {
-  const {addNewTodo} = props;
-  addNewTodo("tientho");
+  const { addNewTodo } = props;
+  // addNewTodo("tientho");
 
-    return (
-        <div className="todo-new">
-        <input type="text"></input>
-        <button>Add</button>
-      </div>
-    )
+  const handleOnClick = () => {
+    alert("click me");
+  }
+
+  const handleOnChange = (name) => {
+    console.log(">>> handleOnChange", name)
+  }
+
+  return (
+    <div className="todo-new">
+      <input type="text"
+        onChange={(event) => handleOnChange(event.target.value)}
+      ></input>
+      <button
+        style={{ cursor: "pointer" }}
+        onClick={handleOnClick}
+      >Add</button>
+    </div>
+  )
 }
 
 export default TodoNew;
