1e43d1a7118e3dd20c9126145eb8896b3fc27bbc
Phạm Tiến Thọ <tientho1420@gmail.com>
2025-12-03 16:55:24 +0700

    video-82: hiển thị avatar và nút bấm upload file

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -12,18 +12,19 @@ const ViewUserDetail = (props) => {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
 
-    useEffect( () => {
-        if(userDetail) {
+    useEffect(() => {
+        if (userDetail) {
             setId(userDetail._id);
             setFullName(userDetail.fullName);
             setEmail(userDetail.email);
             setPhone(userDetail.phone);
         }
-    })
+    }, [userDetail])
 
     return (
         <div>
             <Drawer
+                width={"40vw"}
                 title="Thông tin chi tiết người dùng"
                 closable={{ 'aria-label': 'Close Button' }}
                 onClose={() => {
@@ -37,6 +38,26 @@ const ViewUserDetail = (props) => {
                     <div>Full name: {fullName}</div>
                     <div>Email: {email}</div>
                     <div>Phone number: {phone}</div>
+                    <div>
+                        <img
+                            style={{ width: "170px" }}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${userDetail?.avatar}`}
+                        />
+                    </div>
+                    <label htmlFor="uploadAvatarBtn"
+                        style={{
+                            display: "block",
+                            width: "fit-content",
+                            marginTop: "15px",
+                            padding: "5px 10px",
+                            background: "white",
+                            borderRadius: "3px",
+                            cursor: "pointer",
+                            color: "orange",
+                            border: "solid 2px orange"
+                        }}
+                    >Upload File</label>
+                    <input type="file" hidden id="uploadAvatarBtn" />
                 </div>
             </Drawer>
 
